576aa38619218ae6f31e9550be272c60d68ef752
derick_vitor <derick.castro@primecontrol.com.br>
2025-03-06 09:49:46 -0300

    del exe ps1 files

--- run.ps1
+++
@@ -1,7 +0,0 @@
-$venvPath = ".\.venv"
-if (Test-Path "$venvPath") {
-    . "$venvPath\Scripts\Activate.ps1"
-    python main.py
-} else {
-    Write-Host "Virtual environment not found. Run setup.ps1 first."
-}
